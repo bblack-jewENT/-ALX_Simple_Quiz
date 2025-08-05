864ccb5ab9ee90b47d929a80d7e2ff5246c81a7e
Moses Rametsi <mosesrametsi16@gmail.com>
2025-08-05 22:02:11 +0200

    Add event listener for the add function.

--- calculator.js
+++ calculator.js
@@ -7,6 +7,15 @@ function add(number1, number2) {
   return  number1 + number2;
 }
 
+add.addEventListener('click', () => {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+
+    const result = add(number2, number2);
+    document.getElementById('calculation-result').textContent = result;
+
+}); 
+
 function subtract(number1, number2) {
     return number1 - number2;
 }
