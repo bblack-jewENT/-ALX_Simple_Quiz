fdffa3928d2d80b7bc17fdd1a09fd41672c38c2a
Moses Rametsi <mosesrametsi16@gmail.com>
2025-08-05 22:00:09 +0200

    Add functions for arithmetic.

--- calculator.js
+++ calculator.js
@@ -2,3 +2,19 @@ const add = document.getElementById('add');
 const subtract = document.getElementById('subtract');
 const multiply = document.getElementById('multiply');
 const divide = document.getElementById('divide');
+
+function add(number1, number2) {
+  return  number1 + number2;
+}
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    return number1 / number2;
+}
