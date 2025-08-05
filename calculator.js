3477298fdc175cd79af65a54271ae9e97dd53121
Moses Rametsi <mosesrametsi16@gmail.com>
2025-08-05 22:20:58 +0200

    Add event listener for the multiply function.

--- calculator.js
+++ calculator.js
@@ -33,6 +33,15 @@ function multiply(number1, number2) {
     return number1 * number2;
 }
 
+multiply.addEventListener('click', () => {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+
+    const result = multiply(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+
+});
+
 function divide(number1, number2) {
     return number1 / number2;
 }
