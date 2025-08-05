e416663c7c74155a2151bbcd3b4317bdd2c0b802
Moses Rametsi <mosesrametsi16@gmail.com>
2025-08-05 16:45:53 +0200

    Select elements by id.

---
+++ calculator.js
@@ -0,0 +1,4 @@
+const add = document.getElementById('add');
+const subtract = document.getElementById('subtract');
+const multiply = document.getElementById('multiply');
+const divide = document.getElementById('divide');
